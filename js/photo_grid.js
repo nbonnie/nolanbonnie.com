5b0596a79893855c29c3265c5c3cae5bbf6c9571
Nolan <nbonnie@sandia.gov>
2018-09-05 17:11:51 -0700

    add photo selection border

--- js/photo_grid.js
+++ js/photo_grid.js
@@ -1,4 +1,6 @@
 $(".grid_photo").click(function() {
     var src = $(this).prop("src");
-   $("#display_photo").prop("src",src)
+    $("#display_photo").prop("src",src)
+    $(".grid_photo").css("border","none")
+    $(this).css("border","5px solid #F73859")
 });
