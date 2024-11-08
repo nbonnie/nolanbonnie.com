67ebdcdd77fb5324b6981b783ae4aef0b99c8bb1
nolanrbonnie@gmail.com <nolanrbonnie@gmail.com>
2024-11-08 04:10:42 -0700

    added rename images script

---
+++ portfolio/rename_images.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Navigate to the folder containing the images
+cd /path/to/your/folder
+
+# Initialize a counter variable
+counter=1
+
+# Loop through all .jpg files in the folder
+for img in *.jpg; do
+    # Format the counter to be 3 digits, padding with leading zeros
+    new_name=$(printf "%03d.jpg" $counter)
+
+    # Rename the file
+    mv "$img" "$new_name"
+
+    # Increment the counter
+    ((counter++))
+done
+
+echo "Renaming complete!"
+
